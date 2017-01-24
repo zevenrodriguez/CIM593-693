8df65b494f60dea651f70618876fc8fb159eda79
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-01-24 16:39:15 -0500

    class week2

---
+++ week2/examples/classExamples/javascriptExample/js/script.js
@@ -0,0 +1,35 @@
+var num = 100;
+var num1 = 58;
+var word = "hello";
+var word1 = "goodbye"
+
+
+console.log(num);
+
+console.log(num + num1);
+
+console.log(word + word1);
+
+console.log(word + num);
+
+//console.log(word - num);
+
+
+//Single comment
+
+/*
+    This is 
+    a 
+    long
+    Comment
+*/
+
+console.log(document.getElementById("box1"));
+
+document.getElementById("box1").style.fontSize = "25px";
+
+document.getElementById("box2").innerHTML = "Hello";
+
+var box3 = document.getElementById("box3");
+
+box3.style.display = "none";
