d91dfa94b9f2073e140f51d246d322505a705d20
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-01-30 15:52:01 -0500

    fixing

---
+++ week3/examples/hapiBlipp/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+
+const server = new Hapi.Server();
+server.connection({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        //console.log(request);
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}/{lastname}',
+    handler: function (request, reply) {
+        //console.log(request);
+        reply('Hello, ' + encodeURIComponent(request.params.name) + " " + encodeURIComponent(request.params.lastname) + '!');
+    }
+});
+
+server.register([Blipp], (err) => {
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        console.log(`Server running at: ${server.info.uri}`);
+
+    });
+});
