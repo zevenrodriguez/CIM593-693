3fa61bf58a16d3265e21d7dedbfad3c06448892d
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-02-08 15:29:30 -0500

    week5

---
+++ week5/monstermash/app.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const Vision = require('vision');
+const Inert = require('inert');
+const Path = require('path');
+const Handlebars = require('handlebars');
+
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
+
+server.connection({
+    port: 3000
+});
+
+server.register([Blipp, Inert, Vision], () => {});
+
+server.views({
+    engines: {
+        html: Handlebars
+    },
+    path: 'views',
+    layoutPath: 'views/layout',
+    layout: 'layout',
+    helpersPath: 'views/helpers',
+    //partialsPath: 'views/partials'
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        view: {
+            template: 'index'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: './',
+            listing: false,
+            index: false
+        }
+    }
+});
+
+server.route({
+
+    method: 'POST',
+    path: '/form',
+    handler: function (request, reply) {
+        var formresponse = JSON.stringify(request.payload);
+        //console.log(formresponse.length);
+
+
+        reply.view('formresponse', {
+            formresponse: formresponse
+        });
+    }
+});
+
+server.route({
+
+    method: 'POST',
+    path: '/formJSON',
+    handler: function (request, reply) {
+        var jsonForm = JSON.stringify(request.payload);
+        console.log(jsonForm);
+        reply(jsonForm);
+    }
+
+
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+
+});
