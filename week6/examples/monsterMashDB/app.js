439b0ccca7756d3983882ee28ba52795ded1c837
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-02-21 13:53:31 -0500

    week6

---
+++ week6/examples/monsterMashDB/app.js
@@ -0,0 +1,256 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const Vision = require('vision');
+const Inert = require('inert');
+const Path = require('path');
+const Handlebars = require('handlebars');
+
+const fs = require("fs");
+
+const Sequelize = require('sequelize');
+
+
+
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
+
+server.connection({
+    port: 3000
+});
+
+
+var sequelize = new Sequelize('db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+    // SQLite only
+    storage: 'db.sqlite'
+});
+
+
+var Monster = sequelize.define('monster', {
+    monsterName: {
+        type: Sequelize.STRING
+    },
+    quarters: {
+        type: Sequelize.STRING
+    },
+    weapon: {
+        type: Sequelize.STRING
+    },
+    message: {
+        type: Sequelize.STRING
+    },
+});
+
+
+server.register([Blipp, Inert, Vision], () => {});
+
+server.views({
+    engines: {
+        html: Handlebars
+    },
+    path: 'views',
+    layoutPath: 'views/layout',
+    layout: 'layout',
+    helpersPath: 'views/helpers',
+    //partialsPath: 'views/partials'
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        view: {
+            template: 'index'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: './',
+            listing: false,
+            index: false
+        }
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/createDB',
+    handler: function (request, reply) {
+        // force: true will drop the table if it already exists
+        Monster.sync({
+            force: true
+        })
+        reply("Database Created")
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/createMonster',
+    handler: {
+        view: {
+            template: 'createmonster'
+        }
+    }
+});
+
+server.route({
+
+    method: 'POST',
+    path: '/formMonster',
+    handler: function (request, reply) {
+        var formresponse = JSON.stringify(request.payload);
+        var parsing = JSON.parse(formresponse);
+        //console.log(parsing);
+
+        Monster.create(parsing).then(function (currentMonster) {
+            Monster.sync();
+            console.log("...syncing");
+            console.log(currentMonster);
+            return (currentMonster);
+        }).then(function (currentMonster) {
+
+            reply.view('formresponse', {
+                formresponse: currentMonster
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/displayAll',
+    handler: function (request, reply) {
+        Monster.findAll().then(function (users) {
+            // projects will be an array of all User instances
+            //console.log(users[0].monsterName);
+            var allUsers = JSON.stringify(users);
+            reply.view('dbresponse', {
+                dbresponse: allUsers
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/destroyAll',
+    handler: function (request, reply) {
+
+        Monster.drop();
+
+        reply("destroy all");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/delete/{id}',
+    handler: function (request, reply) {
+
+
+        Monster.destroy({
+            where: {
+                id: encodeURIComponent(request.params.id)
+            }
+        });
+
+        reply().redirect("/displayAll");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/find/{monsterName}',
+    handler: function (request, reply) {
+        Monster.findOne({
+            where: {
+                monsterName: encodeURIComponent(request.params.monsterName),
+            }
+        }).then(function (user) {
+            var currentUser = "";
+            currentUser = JSON.stringify(user);
+            //console.log(currentUser);
+            currentUser = JSON.parse(currentUser);
+            console.log(currentUser);
+            reply.view('find', {
+                dbresponse: currentUser
+            });
+
+        });
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/update/{id}',
+    handler: function (request, reply) {
+        var id = encodeURIComponent(request.params.id);
+
+
+        reply.view('updatemonster', {
+            routeId: id
+        });
+    }
+
+});
+
+server.route({
+    method: 'POST',
+    path: '/update/{id}',
+    handler: function (request, reply) {
+        var cm = "";
+        var id = encodeURIComponent(request.params.id);
+        var formresponse = JSON.stringify(request.payload);
+        var parsing = JSON.parse(formresponse);
+        //console.log(parsing);
+
+        Monster.update(parsing, {
+            where: {
+                id: id
+            }
+        });
+
+        reply().redirect("/displayAll");
+
+    }
+
+});
+
+
+
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+
+});
