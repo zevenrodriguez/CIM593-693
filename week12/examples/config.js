2c2fc564a407b6092d6da0b2f96759e0973d3359
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-04-18 15:05:15 -0400

    config.js example

---
+++ week12/examples/config.js
@@ -0,0 +1,15 @@
+// in your app.js add var config = require('./config.js');
+// remember to add config.js to your gitignore file in the directory
+
+
+ var config = {
+
+     "email": "add email here",
+     "password": "add password",
+     "client_id": "client id",
+     "client_secret": "client secret"
+ }
+
+
+
+module.exports = config;
